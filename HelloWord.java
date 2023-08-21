e0a20864b018c403e52cace4c864871d7fb59494
Aresh N <126131176+AreshNatarajan@users.noreply.github.com>
2023-08-21 18:31:55 +0530

    First file

---
+++ HelloWord.java
@@ -0,0 +1,12 @@
+package org.print;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		System.out.println("wejdwendwkj");
+
+	}
+
+}
